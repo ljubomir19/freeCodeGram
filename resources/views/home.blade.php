fc23fa09e7fc8df416f26b6071253e4fb3dbb4d4
ljubomir19 <djurovic.ljubomir@gmail.com>
2022-02-21 22:55:01 +0100

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3">
+            <img class="rounded-circle p-5" style="width: 200px;" src="https://media.istockphoto.com/photos/very-closeup-view-of-amazing-domestic-pet-in-mirror-round-fashion-is-picture-id1281804798?b=1&k=20&m=1281804798&s=170667a&w=0&h=HIWbeaP_cQSngCz7l9t3xwyE2eyzVgIy3K6xIqPhJQA=" />
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>freeCodeGram</h1></div>
+            <div class="d-flex">
+                <div class="pe-5"><strong>153</strong> posts</div>
+                <div class="pe-5"><strong>23k</strong> folowers</div>
+                <div class="pe-5"><strong>212</strong> folowing</div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
